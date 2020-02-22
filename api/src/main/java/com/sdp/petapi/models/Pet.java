78d7f3fce1eadc340922009944af45e3a4910e97
Conrad Parker <ConradlParker@gmail.com>
2020-02-21 22:15:21 -0600

    Created pet model and some tests
    Signed-off-by: Conrad Parker <ConradlParker@gmail.com>

---
+++ api/src/main/java/com/sdp/petapi/models/Pet.java
@@ -0,0 +1,23 @@
+package com.sdp.petapi.models;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public @Data class Pet {
+    private String name;
+    private String type;
+    private String sex; // M, F
+    private String age; // newborn, young, adult
+    private String size; // small, medium, large, extra large
+    private Double weight;
+    private Date dateAdded;
+    private String description;
+    private String[] imageNames; // link to photos
+    private boolean isAdopted;
+
+}
---
+++ api/src/test/java/com/sdp/petapi/models/PetTest.java
@@ -0,0 +1,29 @@
+package com.sdp.petapi.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Date;
+
+@SpringBootTest
+class PetTest {
+
+	@Test
+	public void create() throws Exception {
+		Date buddy_got_here = new Date();
+		String[] pics_of_buddy = { "walking in the park", "catching a frisbee", "biting the neighbors kid..." };
+		Pet pe = new Pet("Buddy", "dog", "M", "old_af", "medium", 123.4, buddy_got_here,
+				"He is very wet. Just like all the time", pics_of_buddy, false);
+
+		assertEquals(pe.getName(), "Buddy");
+		assertNotEquals(pe, null);
+		assertEquals(pe.getType(), "dog");
+
+	}
+
+}
