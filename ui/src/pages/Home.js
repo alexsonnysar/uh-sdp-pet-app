fba731a1d4917d96d0dfca0d6f9fd2c8bc049411
alexsonnysar <56088949+alexsonnysar@users.noreply.github.com>
2020-02-24 13:34:18 -0600

    Added bootstrap card component template

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PetCard from "../components/PetCard";
 
 const Home = () => {
   return (
     <div>
       <h1>Hello World!!</h1>
+      <PetCard />
     </div>
   );
 };
