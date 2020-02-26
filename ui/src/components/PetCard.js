b2b180e123ab7fb6858583695a131e9c04a4308a
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-02-25 22:30:18 -0600

    Changed card component to not show picture for list

--- ui/src/components/PetCard.js
+++ ui/src/components/PetCard.js
@@ -2,13 +2,19 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-const PetCard = ({ pet }) => {
+const PetCard = ({ pet, ifList }) => {
   // console.log(pet);
   const { Name, Type } = pet;
   return (
     <div data-testid="petcard">
       <Card className="text-center" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="/images/garfield.jpg" alt="Pet Image" />
+        {!ifList ? (
+          <Card.Img
+            variant="bottom"
+            src="/images/garfield.jpg"
+            alt="Pet Image"
+          />
+        ) : null}
         <Card.Body>
           <Card.Title>{Name}</Card.Title>
           <Card.Text>Type: {Type}</Card.Text>
--- ui/src/components/PetList.js
+++ ui/src/components/PetList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PetCard from "./PetCard";
+import CardColumns from "react-bootstrap/CardColumns";
 
 const PetList = ({ heading, petList }) => {
   return (
     <div data-testid="petlist">
       <h2>{heading}</h2>
       {petList.map((pet, index) => (
-        <PetCard pet={pet} key={index} />
+        <CardColumns>
+          <PetCard pet={pet} key={index} ifList={true} />
+        </CardColumns>
       ))}
-      ;
     </div>
   );
 };
