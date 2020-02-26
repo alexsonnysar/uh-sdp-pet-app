4b1984b58d7414b3d840e56127ea675009d59d08
Conrad Parker <ConradlParker@gmail.com>
2020-02-26 15:52:45 -0600

    I lost the PetController and had to go find it
    Signed-off-by: Conrad Parker <ConradlParker@gmail.com>

---
+++ api/src/main/java/com/sdp/petapi/controllers/PetController.java
@@ -0,0 +1,51 @@
+package com.sdp.petapi.controllers;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sdp.petapi.models.Message;
+import com.sdp.petapi.models.Pet;
+import com.sdp.petapi.services.PetService;
+
+@RestController
+@RequestMapping("/pet")
+public class PetController {
+
+  @Autowired
+  private PetService petService;
+
+  // make id required
+  @DeleteMapping("/{id}")
+  public Message deletePet(@PathVariable String id) {
+    return petService.deletePet(id);
+
+  }
+
+  @GetMapping
+  public Collection<Pet> getAllPets() {
+    return petService.getPets();
+  }
+
+  @PostMapping
+  public Pet postPet(@RequestBody Pet pet) {
+    return petService.createPet(pet);
+  }
+
+  @PutMapping("/{id}")
+  public Message putPet(@PathVariable String id, @RequestBody Pet pet) {
+    pet.setId(id);
+    return petService.putPet(pet);
+  }
+}
