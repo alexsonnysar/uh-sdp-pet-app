77a072576cd420434f6f50cbc8d0ef6477c7e4db
ASimpleTouch <victorzapata137@gmail.com>
2020-02-28 01:02:42 -0600

    Adds a new petcontroller delete test

--- api/src/test/java/com/sdp/petapi/controller/PetControllerTest.java
+++ api/src/test/java/com/sdp/petapi/controller/PetControllerTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import com.sdp.petapi.controllers.PetController;
 import com.sdp.petapi.models.Message;
 import com.sdp.petapi.models.Pet;
+import com.sdp.petapi.repositories.PetRepository;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -16,6 +17,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PetControllerTest {
+
+  Pet pet = new Pet("123", "TestingPet", "Dog", "M", "15", "small", 150.0, "2012-04-23T18", "cute", new String[] {"A","B","C"}, false);
 
   @Autowired
   PetController petController;
@@ -26,19 +29,29 @@ class PetControllerTest {
   @Mock
   Message mockMessage;
 
+  @Autowired
+  PetRepository petRepository;
+
   @Test
   public void get_pets_should_return_all_pets() {
     List<Pet> pets = petController.getAllPets();
     assertEquals(anyList(), pets);
   }
 
+
+
+
+
+
   @Test
-  public void pet_should_delete_pet_by_id() {
-    Pet pet = new Pet();
-    pet.setId("001");
-    mockMessage.setMessage("Deleted Pet");
+  public void pet_should_delete() {
+    petRepository.insert(pet);
     Message message = petController.deletePet(pet.getId());
-    assertEquals("Deleted Pet", message.getMessage());
+    assertEquals("Deleted Pet", message.getMessage(), "PetController failed delete by id");
+    // checking if pet object was actually deleted
+    assertEquals(false, petRepository.existsById(pet.getId()), "PetController failed delete by id");
   }
+
+
 
 }
