ad929d9ce22ba8acd3b3696789778e8d68e26776
ASimpleTouch <victorzapata137@gmail.com>
2020-02-29 16:35:56 -0600

    Rewriting PetDaoTest. Adds test for get_all_pets and getPetbyId.

--- api/src/test/java/com/sdp/petapi/dao/PetDaoTest.java
+++ api/src/test/java/com/sdp/petapi/dao/PetDaoTest.java
@@ -6,12 +6,16 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdp.petapi.models.Message;
 import com.sdp.petapi.models.Pet;
+import com.sdp.petapi.repositories.PetRepository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,49 +27,42 @@ public class PetDaoTest {
   @Autowired
   PetDao petDao;
 
+  @Autowired
+  PetRepository petRepository;
+
   @Mock
   Pet expected_pet;
+
+  Pet pet;
 
   @Mock
   Message expected_message;
 
+  @BeforeEach
+  public void init() throws Exception {
+    ObjectMapper om = new ObjectMapper();
+    pet = om.readValue(new File("src/test/java/com/sdp/petapi/resources/mocks/petObject.json"), Pet.class);
+    petRepository.insert(pet);
+  }
+
+  @AfterEach
+  public void cleanup() {
+    petRepository.deleteAll();
+  }
+
+
   @Test
-  public void get_pets_should_work() throws Exception {
+  public void get_all_pets() {
     List<Pet> actual_pets = petDao.getAllPets();
-    assertEquals(new ArrayList<Pet>(), actual_pets);
+    assertEquals(Collections.singletonList(pet), actual_pets);
   }
 
   @Test
-  public void create_pet_should_work() throws Exception {
-    ObjectMapper om = new ObjectMapper();
-    Pet pet = om.readValue(new File("src/test/java/com/sdp/petapi/resources/mocks/petObject.json"), Pet.class);
-
-    Pet actual_pet = petDao.createPet(pet);
-
-    assertEquals(pet.hashCode(), actual_pet.hashCode());
-
+  public void get_pet_by_id() {
+    Pet actual_pet = petDao.getPetById(pet.getId());
+    assertEquals(pet, actual_pet);
   }
 
-  @Test
-  public void delete_pet_should_work() throws Exception {
-    ObjectMapper om = new ObjectMapper();
-    Pet pet = om.readValue(new File("src/test/java/com/sdp/petapi/resources/mocks/petObject.json"), Pet.class);
-
-    Message del_msg = new Message("Deleted Pet");
-    Message actual_message = petDao.deletePet(pet.getId());
-
-    assertEquals(del_msg, actual_message);
-
-  }
-
-  @Test
-  public void put_pet_should_work() throws Exception {
-    ObjectMapper om = new ObjectMapper();
-    Pet pet = om.readValue(new File("src/test/java/com/sdp/petapi/resources/mocks/petObject.json"), Pet.class);
-    pet.setAge("He old now");
-    Message update_msg = new Message("Updated Pet");
-    Message actual_message = petDao.putPet(pet);
-    assertEquals(update_msg, actual_message);
-  }
+ 
 
 }
