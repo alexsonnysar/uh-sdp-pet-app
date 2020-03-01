fc4d882c6803bcfa00ec91fee2d515d1cdc6588e
ASimpleTouch <victorzapata137@gmail.com>
2020-02-29 20:35:59 -0600

    Fixes tests for PetDao and PetService

--- api/src/test/java/com/sdp/petapi/services/PetServiceTest.java
+++ api/src/test/java/com/sdp/petapi/services/PetServiceTest.java
@@ -3,77 +3,80 @@ package com.sdp.petapi.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdp.petapi.dao.PetDao;
-import com.sdp.petapi.models.Message;
 import com.sdp.petapi.models.Pet;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PetServiceTest {
-
-  @BeforeEach
-  void initializeMockForEachTest() {
-    MockitoAnnotations.initMocks(this);
-  }
-
-  @Mock
-  Pet expected_pet;
-
-  @Autowired
-  PetService petService;
+  
+  Pet pet;
 
   @Mock
   PetDao petDao;
 
-  @Mock
-  Message expected_message;
+  // makes a petService whose petDao is the mock above
+  @InjectMocks
+  PetService petService;
 
-  @Mock
-  List<Pet> expected_collection_of_pets;
+  @BeforeEach
+  public void init() throws Exception {
+    ObjectMapper om = new ObjectMapper();
+    pet = om.readValue(new File("src/test/java/com/sdp/petapi/resources/mocks/petObject.json"), Pet.class);
+  }
 
-  @Test
-  public void creates_pet_should_work() throws Exception {
-    when(petService.createPet(expected_pet)).thenReturn(expected_pet);
-
-    Pet actual_pet = petService.createPet(expected_pet);
-
-    assertEquals(expected_pet, actual_pet);
+  @AfterEach
+  public void cleanup() {
   }
 
   @Test
-  public void delete_pet_should_work() throws Exception {
-    when(petService.deletePet(expected_pet.getId())).thenReturn(expected_message);
-
-    Message actual_message = petService.deletePet(expected_pet.getId());
-
-    assertEquals(expected_message, actual_message);
+  public void get_all_pets() {
+    // Since the petDao is a mock it will return null on method calls, so
+    // we must specify what it will return given a specific method call
+    when(petDao.getAllPets()).thenReturn(Collections.singletonList(pet));
+    List<Pet> list = petService.getAllPets();
+    assertEquals(Collections.singletonList(pet), list);
   }
 
   @Test
-  public void read_pet_should_work() throws Exception {
-
-    when(petService.getAllPets()).thenReturn(expected_collection_of_pets);
-
-    List<Pet> actual_collection_of_pets = petService.getAllPets();
-
-    assertEquals(expected_collection_of_pets, actual_collection_of_pets);
+  public void get_pet_by_id() {
+    when(petDao.getPetById(pet.getId())).thenReturn(pet);
+    Pet returnPet = petService.getPetById(pet.getId());
+    assertEquals(pet, returnPet);
   }
 
   @Test
-  public void update_pet_should_work() throws Exception {
-
-    when(petService.putPet(expected_pet)).thenReturn(expected_message);
-
-    Message actual_message = petService.putPet(expected_pet);
-
-    assertEquals(expected_message, actual_message);
+  public void create_pet() {
+    when(petDao.createPet(pet)).thenReturn(pet);
+    Pet returnPet = petService.createPet(pet);
+    assertEquals(pet, returnPet);
   }
+
+
+  
+  @Test
+  public void put_pet() {
+    when(petDao.putPet(pet)).thenReturn(pet);
+    Pet returnedPet = petService.putPet(pet);
+    assertEquals(pet, returnedPet);
+  }
+
+  @Test
+  public void delete_pet() {
+    when(petDao.deletePet(pet.getId())).thenReturn(true);
+    Boolean deleteSuccess = petDao.deletePet(pet.getId());
+    assertEquals(true, deleteSuccess);
+  }
+
 }
