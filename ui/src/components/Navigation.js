563e1cc1ec9e2bc4d1e51c60b5d08bf3bdc8d439
alexsonnysar <56088949+alexsonnysar@users.noreply.github.com>
2020-03-02 15:54:00 -0600

    Test

--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -1,17 +1,47 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 const Navigation = () => {
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      {/* <Navbar bg="primary" variant="dark">
         <Navbar.Brand>UH Pet App</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/employee-dashboard">Employee Dashboard</Nav.Link>
         </Nav>
-      </Navbar>
+      </Navbar> */}
+
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            UH Pet App
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button> */}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
