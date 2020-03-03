2a80a1423d7ced9b84fc04c959b01b31b1c7dda4
unknown <mikhatkhatay@gmail.com>
2020-03-03 17:08:44 -0600

    updated PetTest. all tests pass

--- api/src/test/java/com/sdp/petapi/models/PetTest.java
+++ api/src/test/java/com/sdp/petapi/models/PetTest.java
@@ -7,38 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.*;
 import java.util.stream.*;
 
-import java.util.*;
-
 @SpringBootTest
 class PetTest {
 
   @Test
   public void create() throws Exception {
-<<<<<<< Updated upstream
-    Date buddy_got_here = new Date();
-    ArrayList<String> pics_of_buddy = new ArrayList<String>();
-    pics_of_buddy.add("walking in the park");
-    pics_of_buddy.add("catching a frisbee");
-    pics_of_buddy.add("biting the neighbors kid...");
-    Pet sample_pet = new Pet("Buddy", "dog", "M", "old_af", "medium", 123.4,
-        "He is very wet. Just like all the time", pics_of_buddy);
-    Date now = new Date();
-
-    assertEquals(sample_pet.getName(), "Buddy");
-    assertNotEquals(sample_pet, null);
-    assertEquals(sample_pet.getType(), "dog");
-    assertAll(() -> assertEquals(
-      sample_pet.getName(), "Buddy"),
-      () -> assertEquals(sample_pet.getType(), "dog"),
-      () -> assertEquals(sample_pet.getDateAdded().compareTo(buddy_got_here)/Math.abs(sample_pet.getDateAdded().compareTo(buddy_got_here)),1),
-      () -> assertEquals(sample_pet.getDateAdded().compareTo(now)/Math.abs(sample_pet.getDateAdded().compareTo(now)),-1)
-    );
-=======
     ArrayList<String> petPics =  new ArrayList<String>();
     petPics.add("walking in the park");
     petPics.add("catching a frisbee");
     petPics.add("biting the neighbors kid...");
->>>>>>> Stashed changes
 
     Pet sample_pet = new Pet( "009", "Buddy", "dog", "M", "old_af", "medium", 123.4, new Date(),
         "He is very wet. Just like all the time", petPics.stream().collect(Collectors.toSet()), false, true);
@@ -49,6 +26,31 @@ class PetTest {
       () -> assertEquals(sample_pet.getType(), "dog"),
       () -> assertNotEquals(sample_pet.getDateAdded(), null),
       () -> assertEquals(sample_pet.getImageNames(), petPics.stream().collect(Collectors.toSet()))
+    );
+  }
+
+  @Test
+  public void createImagesSameName() throws Exception {
+    ArrayList<String> petPics =  new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+    petPics.add("catching a frisbee");
+
+    ArrayList<String> picsInserted = new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+
+    Pet sample_pet = new Pet( "009", "Buddy", "dog", "M", "old_af", "medium", 123.4, new Date(),
+        "He is very wet. Just like all the time", petPics.stream().collect(Collectors.toSet()), false, true);
+    
+    assertAll(
+      () -> assertNotEquals(sample_pet, null),
+      () -> assertEquals(sample_pet.getName(), "Buddy"),
+      () -> assertEquals(sample_pet.getType(), "dog"),
+      () -> assertNotEquals(sample_pet.getDateAdded(), null),
+      () -> assertTrue(sample_pet.getImageNames().containsAll(picsInserted.stream().collect(Collectors.toSet())))
     );
   }
 
