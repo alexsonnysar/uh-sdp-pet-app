7643a48ed3e8c3baaa5499ecd9a13f3c10336c2e
Ibrahim Khatkhatay <mikhatkhatay@gmail.com>
2020-03-06 03:46:14 -0600

    updated Pet object and Tests

--- api/src/main/java/com/sdp/petapi/models/Pet.java
+++ api/src/main/java/com/sdp/petapi/models/Pet.java
@@ -1,13 +1,18 @@
 package com.sdp.petapi.models;
 
+import java.util.*;
+import java.util.stream.*;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 
+import org.springframework.data.annotation.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 public @Data class Pet {
+  @Id
   private String id;
   private String name;
   private String type;
@@ -15,9 +20,37 @@ public @Data class Pet {
   private String age; // newborn, young, adult
   private String size; // small, medium, large, extra large
   private Double weight;
-  private String dateAdded;
+  private Date dateAdded;
   private String description;
-  private String[] imageNames; // link to photos
-  private boolean isAdopted;
+  private Set<String> imageNames; // link to photos
+  private boolean adopted;
+  private boolean active;
+
+  public Pet(String name, String type, String sex, String age, String size, double weight, String description,
+			ArrayList<String> imageNames) {
+        this.name = name;
+        this.type = type;
+        this.sex = sex;
+        this.age = age;
+        this.size = size;
+        this.weight = weight;
+        this.description = description;
+        this.imageNames = imageNames.stream().collect(Collectors.toSet());
+  }
+  
+  public Pet(String id, String name, String type, String sex, String age, String size, Double weight, Date date, String desc, ArrayList<String> images, boolean adopt, boolean status){
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.sex = sex;
+    this.age = age;
+    this.size = size;
+    this.weight = weight;
+    this.dateAdded = date;
+    this.description = desc;
+    this.imageNames = images.stream().collect(Collectors.toSet());
+    this.adopted = adopt;
+    this.active = status;
+  }
 
 }
--- api/src/test/java/com/sdp/petapi/models/PetTest.java
+++ api/src/test/java/com/sdp/petapi/models/PetTest.java
@@ -4,21 +4,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.*;
+import java.util.stream.*;
+import java.text.SimpleDateFormat;
 
 @SpringBootTest
 class PetTest {
 
   @Test
   public void create() throws Exception {
-    String[] pics_of_buddy = { "walking in the park", "catching a frisbee", "biting the neighbors kid..." };
-    Pet sample_pet = new Pet("001", "Buddy", "dog", "M", "old_af", "medium", 123.4, "2020-02-26T00:16:17.472-0600",
-        "He is very wet. Just like all the time", pics_of_buddy, false);
+    ArrayList<String> petPics =  new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+    Date sample_date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("26-FEB-2020 18:16:17");
 
-    assertEquals(sample_pet.getName(), "Buddy");
-    assertNotEquals(sample_pet, null);
-    assertEquals(sample_pet.getType(), "dog");
-    assertAll(() -> assertEquals(sample_pet.getName(), "Buddy"), () -> assertEquals(sample_pet.getType(), "dog"));
+    Pet sample_pet = new Pet( "009", "Buddy", "dog", "M", "old_af", "medium", 123.4, sample_date,
+        "He is very wet. Just like all the time", petPics.stream().collect(Collectors.toSet()), false, true);
+    
+    assertAll(
+      () -> assertNotEquals(sample_pet, null),
+      () -> assertEquals(sample_pet.getName(), "Buddy"),
+      () -> assertEquals(sample_pet.getType(), "dog"),
+      () -> assertEquals(sample_pet.getDateAdded(), sample_date),
+      () -> assertEquals(sample_pet.getImageNames(), petPics.stream().collect(Collectors.toSet()))
+    );
+  }
 
+  @Test
+  public void createImagesSameName() throws Exception {
+    ArrayList<String> petPics =  new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+    petPics.add("catching a frisbee");
+    Date sample_date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("26-FEB-2020 18:16:17");
+
+    ArrayList<String> picsInserted = new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+
+    Pet sample_pet = new Pet( "009", "Buddy", "dog", "M", "old_af", "medium", 123.4, sample_date,
+        "He is very wet. Just like all the time", petPics.stream().collect(Collectors.toSet()), false, true);
+    
+    assertAll(
+      () -> assertNotEquals(sample_pet, null),
+      () -> assertEquals(sample_pet.getName(), "Buddy"),
+      () -> assertEquals(sample_pet.getType(), "dog"),
+      () -> assertEquals(sample_pet.getDateAdded(), sample_date),
+      () -> assertTrue(sample_pet.getImageNames().containsAll(picsInserted.stream().collect(Collectors.toSet())))
+    );
   }
 
 }
