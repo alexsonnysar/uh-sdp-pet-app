7917c6808c54c681fd7a26280327c4321795142d
Ibrahim Khatkhatay <mikhatkhatay@gmail.com>
2020-03-06 04:09:24 -0600

    Updated tests to cover additional Pet Constructors

--- api/src/test/java/com/sdp/petapi/models/PetTest.java
+++ api/src/test/java/com/sdp/petapi/models/PetTest.java
@@ -41,9 +41,9 @@ class PetTest {
     Date sample_date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("26-FEB-2020 18:16:17");
 
     ArrayList<String> picsInserted = new ArrayList<String>();
-    petPics.add("walking in the park");
-    petPics.add("catching a frisbee");
-    petPics.add("biting the neighbors kid...");
+    picsInserted.add("walking in the park");
+    picsInserted.add("catching a frisbee");
+    picsInserted.add("biting the neighbors kid...");
 
     Pet sample_pet = new Pet( "009", "Buddy", "dog", "M", "old_af", "medium", 123.4, sample_date,
         "He is very wet. Just like all the time", petPics.stream().collect(Collectors.toSet()), false, true);
@@ -53,7 +53,73 @@ class PetTest {
       () -> assertEquals(sample_pet.getName(), "Buddy"),
       () -> assertEquals(sample_pet.getType(), "dog"),
       () -> assertEquals(sample_pet.getDateAdded(), sample_date),
-      () -> assertTrue(sample_pet.getImageNames().containsAll(picsInserted.stream().collect(Collectors.toSet())))
+      () -> assertEquals(sample_pet.getImageNames(), picsInserted.stream().collect(Collectors.toSet()))
+    );
+  }
+
+  @Test
+  public void eight_argument_constructor() throws Exception {
+    String name = "Buddy";
+    String type = "dog";
+    String sex = "M";
+    String age = "old_af";
+    String size = "medium";
+    double weight = 123.4;
+    String description = "He is very wet. Just like all the time";
+    ArrayList<String> petPics = new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+
+    Pet pet = new Pet(name, type, sex, age, size, weight, description, petPics);
+    assertAll(
+      () -> assertNull(pet.getId()),
+      () -> assertEquals(pet.getName(), name),
+      () -> assertEquals(pet.getType(), type),
+      () -> assertEquals(pet.getSex(), sex),
+      () -> assertEquals(pet.getAge(), age),
+      () -> assertEquals(pet.getSize(), size),
+      () -> assertEquals(pet.getWeight(), weight),
+      () -> assertNull(pet.getDateAdded()),
+      () -> assertEquals(pet.getDescription(), description),
+      () -> assertEquals(pet.getImageNames(), petPics.stream().collect(Collectors.toSet())),
+      () -> assertFalse(pet.isActive()),
+      () -> assertFalse(pet.isAdopted())
+    );
+  }
+
+  @Test
+  public void twelve_argument_constructor() throws Exception {
+    String id = "009";
+    String name = "Buddy";
+    String type = "dog";
+    String sex = "M";
+    String age = "old_af";
+    String size = "medium";
+    double weight = 123.4;
+    Date date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("26-FEB-2020 18:16:17");
+    String description = "He is very wet. Just like all the time";
+    ArrayList<String> petPics = new ArrayList<String>();
+    petPics.add("walking in the park");
+    petPics.add("catching a frisbee");
+    petPics.add("biting the neighbors kid...");
+    Boolean adopt = false;
+    Boolean active = true;
+
+    Pet pet = new Pet(id, name, type, sex, age, size, weight, date, description, petPics, adopt, active);
+    assertAll(
+      () -> assertEquals(pet.getId(), id),
+      () -> assertEquals(pet.getName(), name),
+      () -> assertEquals(pet.getType(), type),
+      () -> assertEquals(pet.getSex(), sex),
+      () -> assertEquals(pet.getAge(), age),
+      () -> assertEquals(pet.getSize(), size),
+      () -> assertEquals(pet.getWeight(), weight),
+      () -> assertEquals(pet.getDateAdded(), date),
+      () -> assertEquals(pet.getDescription(), description),
+      () -> assertEquals(pet.getImageNames(), petPics.stream().collect(Collectors.toSet())),
+      () -> assertTrue(pet.isActive()),
+      () -> assertFalse(pet.isAdopted())
     );
   }
 
