ff2779e32f0e347b1d322849e6813498c99da120
pbyrnes01 <44732273+pbyrnes01@users.noreply.github.com>
2020-03-07 19:57:46 -0600

    completed user crud func and testing, add some 'requested' classes

---
+++ api/src/main/java/com/sdp/petapi/services/UserService.java
@@ -0,0 +1,37 @@
+package com.sdp.petapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sdp.petapi.dao.UserDao;
+import com.sdp.petapi.models.User;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserDao UserDao;
+
+  public List<User> getAllUsers() {
+    return UserDao.getAllUsers();
+  }
+
+  public User getUserById(String id) {
+    return UserDao.getUserById(id);
+  }
+
+  public User createUser(User user) {
+    return UserDao.createUser(user);
+  }
+
+  public User putUser(User user) {
+    return UserDao.putUser(user);
+  }
+
+  public Boolean deleteUser(String id) {
+    return UserDao.deleteUser(id);
+  }
+
+}
--- api/src/test/java/com/sdp/petapi/models/PetTest.java
+++ api/src/test/java/com/sdp/petapi/models/PetTest.java
@@ -12,7 +12,7 @@ class PetTest {
   public void create() throws Exception {
     String[] pics_of_buddy = { "walking in the park", "catching a frisbee", "biting the neighbors kid..." };
     Pet sample_pet = new Pet("001", "Buddy", "dog", "M", "old_af", "medium", 123.4, "2020-02-26T00:16:17.472-0600",
-        "He is very wet. Just like all the time", pics_of_buddy, false);
+        "He is very wet. Just like all the time", pics_of_buddy, false, "Available");
 
     assertEquals(sample_pet.getName(), "Buddy");
     assertNotEquals(sample_pet, null);
