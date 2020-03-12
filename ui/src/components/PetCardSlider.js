72e7b18a892076a177997416395bc1e0e2954d63
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-03-12 04:05:31 -0500

    Update of previous commit

--- ui/src/components/PetCardSlider.js
+++ ui/src/components/PetCardSlider.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import PetCard from "./PetCard";
 import "./PetCardSlider.css";
 
