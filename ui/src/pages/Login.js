e5a10939b96380c78bb6d58dca04769fc5cf0414
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-03-22 17:40:35 -0500

    Got login component centered in page

--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -1,25 +1,21 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
 const Login = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <Grid container spacing={3} justify="center">
-        <Grid item xs>
-          <Paper>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <LoginForm />
-          {/* <h1 align="center">Hello</h1> */}
-        </Grid>
-        <Grid item xs>
-          <Paper>xs</Paper>
-        </Grid>
-      </Grid>
+    <div className={classes.root}>
+      <LoginForm />
     </div>
   );
 };
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 export default Login;
