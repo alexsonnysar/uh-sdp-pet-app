5a77630115964643b7a864b9b49d510b1ff937bf
alexsonnysar <alexsonnysar@gmail.com>
2020-03-22 16:28:02 -0500

    Created LoginForm component

---
+++ ui/src/components/LoginForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const LoginForm = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <form className={classes.container}>
+        <h1 align="center">Log In</h1>
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          m={20}
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+        <Button variant="outlined" className={classes.button}>
+          Log In
+        </Button>
+        <a className={classes.text}>
+          Don't have an account? Register <a href="/register">here</a>
+        </a>
+      </form>
+    </div>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    "& > *": {
+      margin: theme.spacing(1)
+    },
+    width: "20rem",
+    display: "flex",
+    flexDirection: "column"
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  button: {
+    color: "primary"
+  },
+  text: {
+    textAlign: "center"
+  }
+}));
+
+export default LoginForm;
--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs>
+          <Paper>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <LoginForm />
+          {/* <h1 align="center">Hello</h1> */}
+        </Grid>
+        <Grid item xs>
+          <Paper>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Login;
