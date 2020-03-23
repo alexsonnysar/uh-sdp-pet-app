71764e38c83316ec8e1c5d2b1daef84e14763a6a
Ibrahim Khatkhatay <mikhatkhatay@gmail.com>
2020-03-23 02:53:32 -0500

    Added/Updated RequestsDao and tests

--- api/src/main/java/com/sdp/petapi/dao/RequestsDao.java
+++ api/src/main/java/com/sdp/petapi/dao/RequestsDao.java
@@ -26,37 +26,44 @@ public class RequestsDao {
   }
 
   public Requests getRequestById(String reqid){
+    if (reqid == null) return null;
     Optional<Requests> req = repository.findById(reqid);
     
     return req.isPresent() ? req.get() : null;
   }
 
   public Requests createRequest(User user, String petid) {
-    if(user.isEmployee() || petid == null) return null;
+    if(user == null || user.isEmployee() || petid == null) return null;
 
     User userdb = userDao.getUserById(user.getId());
-    if (userdb != user) return null;
+    if (!user.equals(userdb)) return null;
 
     Pet pet = petDao.getPetById(petid);
     if (pet == null || !pet.isActive()) return null;
+
+    Boolean existing_req = getAllRequests()
+      .stream()
+      .anyMatch(r -> r.getPetid().equals(petid) && r.getUserid().equals(user.getId()) && r.getStatus() != "CANCELED");
+    if (existing_req) return null;
 
     /* Conrad: right now make it so request makes status "PENDING" (happens in constructor) 
        and pet.isAdopted = T and pet.isActive = F */
     pet.setActive(false);
     pet.setAdopted(true);
+    petDao.putPetByRequest(pet);
     return repository.insert(new Requests(user.getId(), petid));
   }
 
   public Requests putRequests(User user, Requests req) {
-    if(user == null || req == null || (!user.isEmployee() && user.getId() != req.getUserid())) return null;
+    if(user == null || req == null || (!user.isEmployee() && !user.getId().equals(req.getUserid()))) return null;
 
     User userdb = userDao.getUserById(user.getId());
-    if (userdb != user) return null;
+    if (!user.equals(userdb)) return null;
 
     Requests reqdb = getRequestById(req.getId());
-    if (reqdb == null || reqdb.getUserid() != req.getUserid()
-      || reqdb.getPetid() != req.getPetid()
-      || reqdb.getRequestDate() != req.getRequestDate()) return null;
+    if (reqdb == null || !reqdb.getUserid().equals(req.getUserid())
+      || !reqdb.getPetid().equals(req.getPetid())
+      || !reqdb.getRequestDate().equals(req.getRequestDate())) return null;
 
     // WebUser canceling request means pet may be available for adoption again
     return (req.getStatus() == "CANCELED") ? cancelRequest(req) :
@@ -73,13 +80,15 @@ public class RequestsDao {
 
   public Requests approveRequest(Requests req) {
     List<Requests> cancelReqs = repository.findAll().stream()
-      .filter(r -> r.getUserid() != req.getUserid() && r.getPetid() == req.getPetid() && r.getStatus() == "PENDING")
+      .filter(r -> !r.getUserid().equals(req.getUserid()) && r.getPetid().equals(req.getPetid()) && r.getStatus().equals("PENDING"))
       .collect(Collectors.toList());
     
-    cancelReqs.forEach(r -> r.setStatus("CANCELED"));
-    repository.saveAll(cancelReqs);
-
-    req.setStatus("APPROVE");
+    if (!cancelReqs.isEmpty()) {
+      cancelReqs.forEach(r -> r.setStatus("CANCELED"));
+      repository.saveAll(cancelReqs);
+    }
+    
+    req.setStatus("APPROVED");
     repository.save(req);
 
     return req;
@@ -88,12 +97,13 @@ public class RequestsDao {
   public Requests cancelRequest(Requests req) {
     String petid = req.getPetid();
     String userid = req.getUserid();
+
     if (!repository.findAll()
       .stream()
       .anyMatch(
-        r -> r.getPetid() == petid
-        && r.getUserid() != userid
-        && r.getStatus() != "CANCELED"
+        r -> r.getPetid().equals(petid)
+        && !r.getUserid().equals(userid)
+        && !r.getStatus().equals("CANCELED")
       )
     ) {
       /* To undo Conrad's earlier comment */
