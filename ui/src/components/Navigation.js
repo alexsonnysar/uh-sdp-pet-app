70f080c9ca544374a5afc1f905ab5796fa5e1d37
alexsonnysar <alexsonnysar@gmail.com>
2020-03-23 16:05:01 -0500

    Removed unused imports from Navigation.js

--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -4,10 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Link from "@material-ui/core/Link";
-import { red } from "@material-ui/core/colors";
 import { useAuth0 } from "../react-auth0-spa";
 
 const Navigation = () => {
