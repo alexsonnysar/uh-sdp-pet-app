0150e5769ce9e87432c7996010b62955d3bd4f00
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-03-28 16:03:40 -0500

    Removed utils directory and removed commented code in App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -41,20 +41,6 @@ function App() {
           </Switch>
         </div>
       </Router>
-      {/* <Router>
-        <Navigation />
-        <div className="App" data-testid="App">
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/user-dashboard" component={UserDashboard} />
-            <Route path="/pet-profile" component={PetProfile} />
-            <Route path="/pet-register" component={RegisterPet} />
-            <Route path="/employee-dashboard" component={EmployeeDashboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </div>
-      </Router> */}
     </div>
   );
 }
