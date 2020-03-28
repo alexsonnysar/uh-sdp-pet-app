e35e5c96e5042ac31d22e0cc47ead1d0d6954e10
Conrad Parker <ConradlParker@gmail.com>
2020-03-28 14:00:54 -0500

    not working
    Signed-off-by: Conrad Parker <ConradlParker@gmail.com>

--- ui/src/components/PetListItem.js
+++ ui/src/components/PetListItem.js
@@ -6,16 +6,32 @@ import ListItemText from "@material-ui/core/ListItemText";
 import DeleteIcon from "@material-ui/icons/Delete";
 import UpdateRoundedIcon from "@material-ui/icons/UpdateRounded";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const PetListItem = ({ pet }) => {
-  const { name, type } = pet;
+  const { name, type, id } = pet;
   const classes = useStyles();
+  const DeletePet = petData => {
+    axios({
+      methodd: 'delete',
+      url: 'http://localhost:8080/pet',
+      data: petData,
+      headers: {'Content-Type':'application/json'}
+    }).then(response => console.log(response))
+    .catch(error => console.log(error));
+  };
+
+  const handleDelete = (id) => {
+    DeletePet(id)
+
+  }
 
   return (
     <ListItemLink href="pet-profile" data-testid="petlistitem">
       <ListItemText primary={name} secondary={type} />
       <ListItemSecondaryAction>
         <Button
+          onClick={(id) => handleDelete()}
           variant="contained"
           className={classes.button}
           color="secondary"
@@ -45,7 +61,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function ListItemLink(props) {
+const ListItemLink = props => {
   return <ListItem button component="a" {...props} />;
 }
 
