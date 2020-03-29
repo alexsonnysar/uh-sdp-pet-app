17c08307e660d6850e7a29e403e6ba481d68fe98
James Le <le.toanjames@gmail.com>
2020-03-28 19:09:53 -0500

    started integration for user registration

--- ui/src/components/RegisterForm.js
+++ ui/src/components/RegisterForm.js
@@ -1,48 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    firstName: "",
+    lastName: ""
+  });
+
+  const PostAddUser = userData => {
+    axios({
+      method: "post",
+      url: "http://localhost:8080/user",
+      data: userData,
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
+
+  const handleChange = e => {
+    console.log(e.target.id);
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = () => {
+    if (formData.password == formData.passwordConfirm) {
+      PostAddUser(formData);
+    } else {
+      alert("Your passwords don't match!");
+    }
+  };
+
   const classes = useStyles();
   return (
     <div data-testid="registerForm">
       <form className={classes.container}>
         <h1 align="center">Register</h1>
         <TextField
-          id="outlined-basic"
+          id="firstName"
           label="First Name"
           variant="outlined"
           m={20}
+          onChange={handleChange}
         />
         <TextField
-          id="outlined-basic"
+          id="lastName"
           label="Last Name"
           variant="outlined"
           m={20}
+          onChange={handleChange}
         />
         <TextField
-          id="outlined-basic"
+          id="email"
           label="Email"
           variant="outlined"
           m={20}
+          onChange={handleChange}
         />
         <TextField
-          id="outlined-password-input"
+          id="password"
           label="Password"
           type="password"
           autoComplete="current-password"
           variant="outlined"
+          onChange={handleChange}
         />
         <TextField
-          id="outlined-password-input"
+          id="passwordConfirm"
           label="Confirm Password"
           type="password"
           autoComplete="current-password"
           variant="outlined"
+          onChange={handleChange}
         />
-        <Button variant="outlined" className={classes.button}>
+        <Button
+          variant="outlined"
+          className={classes.button}
+          onClick={() => handleSubmit()}
+        >
           Complete Registration
         </Button>
         <small className={classes.text}>
