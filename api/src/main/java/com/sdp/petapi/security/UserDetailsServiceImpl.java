ad130cfb905ba02d633de796a338b7e0ad0b8ef3
ASimpleTouch <victorzapata137@gmail.com>
2020-03-30 18:28:08 -0500

    comments

--- api/src/main/java/com/sdp/petapi/security/UserDetailsServiceImpl.java
+++ api/src/main/java/com/sdp/petapi/security/UserDetailsServiceImpl.java
@@ -18,10 +18,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
 	@Transactional
+	// read email from JWT and see if it exists in db
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		User user = userRepository.findByEmail(email)
 				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
 
+		// build(user) will set the user authority based on the boolean value isEmployee
 		return UserDetailsImpl.build(user);
 		// return UserDetailsImpl.build(new User("12345", email, "password", "fname", "lastName", true));
 	}
