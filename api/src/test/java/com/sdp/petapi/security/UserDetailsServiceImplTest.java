990d5bd870d7a4866ff9ff3e28a6862725f85fee
Ibrahim Khatkhatay <mikhatkhatay@gmail.com>
2020-04-01 22:11:42 -0500

    Cleaned up test file

--- api/src/test/java/com/sdp/petapi/security/UserDetailsServiceImplTest.java
+++ api/src/test/java/com/sdp/petapi/security/UserDetailsServiceImplTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
