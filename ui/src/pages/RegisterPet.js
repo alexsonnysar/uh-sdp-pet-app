7cb2c8b57fe4bb3b4ff0fefe92d87d4495986005
James Le <le.toanjames@gmail.com>
2020-04-06 17:27:06 -0500

    fixed redirect issues

--- ui/src/pages/RegisterPet.js
+++ ui/src/pages/RegisterPet.js
@@ -19,9 +19,9 @@ const Register = () => {
       </div>
     );
   } else if (localStorage.getItem('roles') === 'ROLE_User') {
-    window.location.replace('http://localhost:8080/user-dashboard');
+    window.location.replace('http://localhost:3000/user-dashboard');
   } else {
-    window.location.replace('http://localhost:8080/login');
+    window.location.replace('http://localhost:3000/login');
   }
 };
 
