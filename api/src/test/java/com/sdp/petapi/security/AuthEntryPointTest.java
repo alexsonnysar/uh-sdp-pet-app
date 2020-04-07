5650c98e9c68762938f8932da5134c34ea824e80
ASimpleTouch <victorzapata137@gmail.com>
2020-04-06 20:20:12 -0500

    Removes unnecessary dependencies

--- api/src/test/java/com/sdp/petapi/security/AuthEntryPointTest.java
+++ api/src/test/java/com/sdp/petapi/security/AuthEntryPointTest.java
@@ -10,16 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
 class AuthEntryPointTest {
 
     @Autowired
