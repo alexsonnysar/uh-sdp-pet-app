b64fe9a86e3ae2426f2d4000d94d0810913d62c3
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-04-30 15:57:20 -0500

    Added email already in use message for registration, and made email from form to lower case

---
+++ ui/src/tests/components/ErrorReqMsg.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import ErrorReqMsg from '../../components/ErrorReqMsg';
+
+describe('<ErrorReqMsg /> Tests', () => {
+  let mockCallBack;
+  let shallowWrapper;
+
+  beforeEach(() => {
+    mockCallBack = jest.fn();
+    shallowWrapper = shallow(
+      <ErrorReqMsg open errorMsg="Test Msg" handleClose={mockCallBack} />
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('should render the ErrorReqMsg Component', () => {
+    expect(shallowWrapper).toHaveLength(1);
+  });
+
+  test('Check if Snackbar component has been rendered', () => {
+    expect(shallowWrapper.find(Snackbar)).toHaveLength(1);
+  });
+
+  test('Check if Snackbar component has the open prop as true', () => {
+    expect(shallowWrapper.find(Snackbar).prop('open')).toBe(true);
+  });
+
+  test('Check if MuiAlert Component has been rendered', () => {
+    expect(shallowWrapper.find(MuiAlert)).toHaveLength(1);
+  });
+
+  test('Check if MuiAlert Component has correct text', () => {
+    expect(shallowWrapper.find(MuiAlert).text()).toEqual('Test Msg');
+  });
+
+  test('Emulate Click Away at SnackBar Level', () => {
+    shallowWrapper.find(Snackbar).simulate('close');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+
+  test('Emulate Click Away at MuiAlert Level', () => {
+    shallowWrapper.find(MuiAlert).simulate('close');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
